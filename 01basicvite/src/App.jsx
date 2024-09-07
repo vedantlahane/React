b54ecad7096cdb7ca476e865a58d80b7f3e6e57d
Vedant Lahane <vedantanillahane@gmail.com>
2024-09-07 22:58:01 +0530

    Update App.jsx

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const addValue = () => {
+    const newCounter = counter + 1;
+    setCounter(newCounter);
+  };
+
+  const removeValue = () => {
+    setCounter(counter => counter - 1);
+    setCounter(counter - 1);
+  };
+
   return (
     <>
       <h1>Vite react app</h1>
+      <h2>Counter Value : {counter}</h2>
+      <button onClick={addValue}>Add Value</button>{" "}
+      <button onClick={removeValue}>Remove Value</button>
     </>
   );
 }
